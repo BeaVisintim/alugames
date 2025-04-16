313b5c49542b864690c4a389ff5b081ea427943b
Beatriz Visintim <visintimb10@gmail.com>
2025-04-16 19:02:12 -0300

    Botão: confirme devolução

--- js/app.js
+++ js/app.js
@@ -1,6 +1,51 @@
 function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-$(id)`);
-    let imagem = gameClicado.querySelector('dashboard__item__img');
-    let botao = gameClicado.querySelector('dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('dashboard__item__name');
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        const confirmar = confirm('Tem certeza que deseja devolver este jogo?');
+        if (!confirmar) {
+            return;
+        }
+
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+    contarJogosAlugados(); // Atualiza na tela
 }
+
+function contarJogosAlugados() {
+    const alugados = document.querySelectorAll('.dashboard__item__img--rented');
+    const contador = document.getElementById('contador-alugados');
+    contador.textContent = `Jogos alugados: ${alugados.length}`;
+}
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        const confirmar = confirm('Tem certeza que deseja devolver este jogo?');
+        if (!confirmar) {
+            return;
+        }
+
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+}
+
+
